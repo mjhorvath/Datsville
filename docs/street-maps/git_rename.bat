cfa52a845ed48b20edf9cca04bd14675d0f7c187
Michael Horvath <17457030+mjhorvath@users.noreply.github.com>
2021-04-09 15:07:00 -0400

    Fix rename of images.

---
+++ docs/street-maps/git_rename.bat
@@ -0,0 +1,8 @@
+git mv datsville_streetmap_rev003.463.png datsville_streetmap_rev004.463.png
+git mv datsville_streetmap_rev003.472.png datsville_streetmap_rev004.472.png
+git mv datsville_streetmap_rev003.501.png datsville_streetmap_rev004.501.png
+git mv datsville_streetmap_rev003.519.png datsville_streetmap_rev004.519.png
+git mv datsville_streetmap_rev003.527.png datsville_streetmap_rev004.527.png
+git mv datsville_streetmap_rev003.528.png datsville_streetmap_rev004.528.png
+git mv datsville_streetmap_rev003.531.png datsville_streetmap_rev004.531.png
+pause
