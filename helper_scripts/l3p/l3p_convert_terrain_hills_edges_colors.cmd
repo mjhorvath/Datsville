c28b94627a4c576e73e3ce8a00979db9d0cbefbc
Michael Horvath <17457030+mjhorvath@users.noreply.github.com>
2021-04-20 14:54:23 -0400

    Tweak tile letters. Update model table.

---
+++ helper_scripts/l3p/l3p_convert_terrain_hills_edges_colors.cmd
@@ -0,0 +1,3 @@
+set LDRAWDIR=C:\Users\Public\Documents\LDraw
+"D:\PortableApps\l3p\L3P20131202.EXE" "D:\GitHub\Datsville\datsville_source\terrain_hills_edges_colors.ldr" -o "l3p_terrain_hills_edges_colors.pov" -il -ic > l3p_errors.txt
+pause
--- helper_scripts/model_table_generator/LDView_SaveSnapshot_x32.bat
+++ helper_scripts/model_table_generator/LDView_SaveSnapshot_x32.bat
@@ -1,4 +1,4 @@
-set LDVIEW_EXE="C:\Program Files (x86)\LDView\LDView.exe"
-cd "W:\LDraw\datsville\svn\trunk\datsville"
-for %%a in ( "*.dat", "*.ldr", "*.mpd" ) do %LDVIEW_EXE% "%%a" -SaveSnapshot=%%~na.png -PreferenceSet=DatsvilleSVN -SaveActualSize=0 -SaveImageType=1 -SaveZoomToFit=1 -SaveWidth=400 -SaveHeight=300 -DefaultZoom=0.95 -FOV=10 -DefaultLatLong=30,45
+set LDVIEW_EXE="C:\Program Files (x86)\LDraw\LDView\LDView.exe"
+cd "D:\GitHub\Datsville\datsville_source"
+for %%a in ( "*.dat", "*.ldr", "*.mpd", "*.xmpd" ) do %LDVIEW_EXE% "%%a" -SaveSnapshot=%%~na.png -PreferenceSet=Quality -SaveActualSize=0 -SaveImageType=1 -SaveZoomToFit=1 -SaveWidth=400 -SaveHeight=300 -DefaultZoom=0.95 -FOV=10 -DefaultLatLong=30,45
 pause
