3d918a0144167aac1376b613249ddfff1efb7a48
Michael Horvath <17457030+mjhorvath@users.noreply.github.com>
2021-06-19 21:14:20 -0400

    Add parts list. Tweaks to interactive map. Tweaks to LDBoxer scripts.

---
+++ scripts/ldboxer/ldboxer_m6459.bat
@@ -0,0 +1,4 @@
+title LDBoxer - ldboxer_m6459.bat
+python3 "D:\GitHub\LDBoxer\LDBoxer.py" "C:\Users\Public\Documents\LDraw" "m6459.ldr"
+::"D:\PortableApps\pypy3.7-v7.3.5-win64\pypy3.exe" "D:\GitHub\LDBoxer\LDBoxer.py" "C:\Users\Public\Documents\LDraw" "m6459.ldr" -v
+pause
--- scripts/ldboxer/ldboxer_safehouse_tiny.bat
+++
@@ -1,4 +0,0 @@
-title LDBoxer - ldboxer_safehouse_tiny.bat
-::python "E:\Programs Windows\LDraw\LDBoxer-master\LDBoxer.py" "C:\Users\Public\Documents\LDraw" "building_013_safehouse_tiny_boxed_.ldr" -v
-"E:\Programs Windows\pypy2-v5.10.0-win32\pypy.exe" "E:\Programs Windows\LDraw\LDBoxer-master\LDBoxer.py" "C:\Users\Public\Documents\LDraw" "building_013_safehouse_tiny_boxed_n.ldr" -v
-pause
